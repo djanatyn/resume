c66daa71ea3da969f189cef7496186fb70a17d96
Jonathan Strickland <djanatyn@gmail.com>
2022-09-24 14:10:55 -0400

    add icons, link to revisions

--- src/Main.hs
+++ src/Main.hs
@@ -8,13 +8,12 @@
 
 module Main where
 
-import Control.Monad (mapM_)
 import qualified Data.ByteString.Lazy.Char8 as BL
-import Data.Text as T (Text, concat, unpack)
+import Data.Text as T (Text, concat, take, unpack)
+import qualified Data.Text.IO as TIO
 import Dhall (FromDhall, Generic, auto, inputFile)
-import System.IO (writeFile)
 import System.Process.Typed (readProcess_)
-import qualified TH
+import TH (Versions (..), versions)
 import qualified Text.Blaze as B
 import Text.Blaze.Html.Renderer.Pretty (renderHtml)
 import Text.Blaze.Html5 as BH
@@ -47,6 +46,27 @@ data Resume where
     Resume
   deriving (Show, Generic, FromDhall)
 
+data Icons where
+  Icons ::
+    { haskell :: BH.Html,
+      nixos :: BH.Html,
+      rust :: BH.Html,
+      elm :: BH.Html
+    } ->
+    Icons
+
+loadIcon :: FilePath -> IO BH.Html
+loadIcon path = BH.preEscapedToHtml <$> TIO.readFile path
+
+loadIcons :: IO Icons
+loadIcons = do
+  haskell <- loadIcon "icons/haskell.svg"
+  nixos <- loadIcon "icons/nixos.svg"
+  rust <- loadIcon "icons/rust.svg"
+  elm <- loadIcon "icons/elm.svg"
+
+  pure $ Icons {haskell, nixos, rust, elm}
+
 loadResume :: IO Resume
 loadResume = inputFile (auto @Resume) "./experience.dhall"
 
@@ -57,31 +77,36 @@ renderJob Job {organization, position, duration, experiences} = do
     BH.div ! A.class_ "position" $ BH.p $ BH.toHtml position <> BH.toHtml ("(" ++ unpack duration ++ ")")
   BH.ul $ mapM_ (BH.li . BH.toHtml) experiences
 
-versions :: [String]
-versions = $(TH.versions)
-
 main :: IO ()
 main = do
   -- load resume
-  Resume {contact, history} <- loadResume
+  Resume {contact = ContactInfo {name, email, github}, history} <- loadResume
+  Icons {haskell, nixos, rust, elm} <- loadIcons
 
   -- render html
   writeFile "resume.html" $
-    renderHtml $
-      let text = BH.toHtml @String
-       in do
-            BH.h1 $ BH.toHtml $ name contact
-            BH.hr
-            BH.div ! A.class_ "box" $
-              let ContactInfo {name, email, github} = contact
-               in do
-                    BH.div $ BH.a ! A.href (B.textValue github) $ BH.h4 $ BH.toHtml $ github
-                    BH.div $ BH.a ! A.href (B.textValue $ T.concat ["mailto:", email]) $ BH.h4 $ BH.toHtml $ email
-            BH.h3 $ text "Work Experience"
-            mapM_ renderJob history
-            BH.hr
-            BH.h3 ! A.class_ "right" $ text "(this resume built with...)"
-            BH.ul $ mapM_ (BH.li . BH.toHtml) versions
+    renderHtml $ do
+      BH.h1 $ BH.toHtml name
+      BH.hr
+      BH.div ! A.class_ "box" $ do
+        BH.div $ BH.a ! A.href (B.textValue github) $ BH.h4 $ BH.toHtml github
+        BH.div $ haskell >> nixos >> rust >> elm
+        BH.div $ BH.a ! A.href (B.textValue $ T.concat ["mailto:", email]) $ BH.h4 $ BH.toHtml email
+      BH.h3 $ BH.string "Work Experience"
+      mapM_ renderJob history
+      BH.hr
+      BH.h3 ! A.class_ "right" $ do
+        "(this resume "
+        BH.a ! A.href "https://github.com/djanatyn/resume/blob/master/flake.nix" $ "built with..."
+        ")"
+      let Versions {nixpkgsRev, ghcVersion, ghcRev} = $(versions)
+          nixpkgsUrl = T.concat ["https://github.com/NixOS/nixpkgs/tree/", nixpkgsRev]
+          ghcUrl = T.concat ["https://gitlab.haskell.org/ghc/ghc/-/tree/", ghcRev]
+          nixpkgsText = T.concat ["NixOS/nixpkgs (", T.take 8 nixpkgsRev, ")"]
+          ghcText = T.concat ["GHC ", ghcVersion, " (", T.take 8 ghcRev, ")"]
+       in BH.ul $ do
+            BH.li $ BH.a ! A.href (B.textValue nixpkgsUrl) $ B.text nixpkgsText
+            BH.li $ BH.a ! A.href (B.textValue ghcUrl) $ B.text ghcText
 
   -- convert html to pdf
   (stdout, stderr) <- readProcess_ "wkhtmltopdf --user-style-sheet style.css resume.html resume.pdf"
--- src/TH.hs
+++ src/TH.hs
@@ -1,29 +1,35 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module TH where
 
 import qualified Data.ByteString.Lazy.Char8 as BL
+import Data.Text as T (Text, pack, strip)
 import Language.Haskell.TH
 import Language.Haskell.TH.Syntax
 import System.Environment
 import System.Process.Typed
 
+data Versions where
+  Versions ::
+    { nixpkgsRev :: Text,
+      ghcVersion :: Text,
+      ghcRev :: Text
+    } ->
+    Versions
+  deriving (Show)
+
+run :: ProcessConfig stdin stdoutIgnored stderrIgnored -> Q Text
+run cmd = runIO $ T.strip . T.pack . BL.unpack . fst <$> readProcess_ cmd
+
 -- Fetch version information at compile-time using Template Haskell.
 versions :: Q Exp
-versions =
-  let run cmd = runIO $ BL.unpack . fst <$> readProcess_ cmd
-   in do
-        -- passed in during `buildPhase`
-        nixpkgsVersion <- runIO $ getEnv "RESUME_NIXPKGS_REV"
+versions = do
+  -- passed in during `buildPhase`
+  nixpkgsRev <- runIO $ pack <$> getEnv "RESUME_NIXPKGS_REV"
+  ghcRev <- run "ghc --print-project-git-commit-id"
+  ghcVersion <- run "ghc --print-project-version"
 
-        ghcCommit <- run "ghc --print-project-git-commit-id"
-        ghcVersion <- run "ghc --print-project-version"
-
-        [|
-          map
-            unwords
-            [ ["ghc", ghcVersion, "- rev", ghcCommit],
-              ["nixos/nixpkgs - rev", nixpkgsVersion]
-            ]
-          |]
+  [|Versions {nixpkgsRev, ghcRev, ghcVersion}|]
